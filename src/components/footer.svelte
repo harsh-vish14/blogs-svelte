c4c5c9b58eaaed96e2868573891217533c8628c7
harsh-vish14 <harshkv9322@gmail.com>
2023-07-01 21:54:47 +0530

    footer ui improvisied

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-	class="p-10 relative -z-50 h-72 mt-20 flex justify-center items-center"
+	class="p-10 relative -z-50 h-96 mt-20 flex justify-center items-center"
 	style="text-wrap: balance"
 >
 	<div class="font-blod text-4xl w-[600px] text-center quote leading-snug my-10">
@@ -13,6 +13,10 @@
 		you want it to be.
 	</div>
 	<img src={naruto} class="absolute -bottom-10 -left-10 h-60" alt="naruto anime" />
-	<img src={quotation_bottom} class="absolute -bottom-10 right-0 h-20" alt="quotation" />
-	<img src={quotation_top} class="absolute -top-10 left-0 h-20" alt="quotation" />
+	<img
+		src={quotation_bottom}
+		class="absolute md:-bottom-10 -bottom-12 right-0 h-20"
+		alt="quotation"
+	/>
+	<img src={quotation_top} class="absolute md:-top-10 -top-12 left-0 h-20" alt="quotation" />
 </div>
